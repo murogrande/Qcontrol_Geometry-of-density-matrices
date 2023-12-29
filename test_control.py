9e25453125b1937319d8ae60a29f5ba9067b0479
MauroMendizabal <mauro.mendizabal-pico@pasqal.com>
2023-12-29 20:21:37 +0100

    control function out

--- test_control.py
+++ test_control.py
@@ -10,14 +10,14 @@ class TestAlgo(unittest.TestCase):
     def setUp(self) -> None:
         self.vect1 = np.array([0.0, 0.0, 0.9])
         self.vect2 = np.array([0.9, 0.0, 0.0])
+        self.tau = 0.2
 
     def tearDown(self) -> None:
         return super().tearDown()
 
     def test_geodesic(self):
 
-        tau = 0.2
-        georesult = geo.geodesic(tau, self.vect1, self.vect2)
+        georesult = geo.geodesic(self.tau, self.vect1, self.vect2)
         self.assertEqual(georesult[0, 0].real, 0.9092521918410296)
         self.assertEqual(georesult[0, 0].imag, 0.0)
         self.assertEqual(georesult[0, 1].real, 0.12487213325637833)
@@ -31,8 +31,8 @@ class TestAlgo(unittest.TestCase):
         self.assertAlmostEqual(np.trace(georesult).imag, 0.0)
         self.assertAlmostEqual(np.trace(georesult).real, 1.0)
 
-        tau = -0.2
-        georesult = geo.geodesic(tau, self.vect1, self.vect2)
+        
+        georesult = geo.geodesic(-self.tau, self.vect1, self.vect2)
 
         self.assertEqual(georesult[0, 0].real, 0.9567036024784532)
         self.assertEqual(georesult[0, 0].imag, 0.0)
@@ -48,7 +48,7 @@ class TestAlgo(unittest.TestCase):
         self.assertAlmostEqual(np.trace(georesult).real, 1.0)
 
         with self.assertRaises(ValueError):
-            geo.geodesic(tau, self.vect1, np.array([1.0, 0.0, 0.0]))
+            geo.geodesic(self.tau, self.vect1, np.array([1.0, 0.0, 0.0]))
 
     def test_muk(self):
         mukresult = muk.muk(self.vect1, self.vect2)
